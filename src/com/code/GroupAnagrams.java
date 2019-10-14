40cfda8966186f66b38a393f26bd40dd84f4afc9
chong <chd830@naver.com>
2019-10-14 23:14:05 +0900

    hashmap써서 스트링에서 각각의 알파벳의 합고, 곱을 더한값을 통해서 같은값이 몇개나 있는지 확인하는 방식으로 접근해보기

---
+++ src/com/code/GroupAnagrams.java
@@ -0,0 +1,10 @@
+package com.code;
+import java.util.*;
+
+public class GroupAnagrams {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> result = new ArrayList();
+
+        return result;
+    }
+}
