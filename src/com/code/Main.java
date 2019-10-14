66a4ec737c413c2ab42e512c1230e120d71878dc
chong <chd830@naver.com>
2019-10-14 13:58:02 +0900

    rotate image

--- src/com/code/Main.java
+++ src/com/code/Main.java
@@ -3,13 +3,8 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Permutations p = new Permutations();
-        List<List<Integer>> result = p.permute(new int[] {1,2,3});
-        for(List l : result ) {
-            for(int i = 0; i < l.size(); i++) {
-                System.out.print(l.get(i)+"\t");
-            }
-            System.out.println("");
-        }
+        RotateImage r = new RotateImage();
+        int[][] matrix = {{1,2,3},{4,5,6},{7,8,9}};
+        r.rotate(matrix);
     }
 }
---
+++ src/com/code/RotateImage.java
@@ -0,0 +1,24 @@
+package com.code;
+
+public class RotateImage {
+    public void rotate(int[][] matrix) {
+        int len = matrix.length;
+        int[][] copy = new int[len][len];
+        for(int i = 0; i < len; i++) {
+            for(int j = 0; j < len; j++) {
+                copy[i][j] = matrix[i][j];
+            }
+        }
+        for(int i = 0, a = len - 1; i < len; i++, a--) {
+            for(int j = 0, b = 0; j < len; b++, j++) {
+                matrix[b][a] = copy[i][j];
+            }
+        }
+        for(int i = 0; i < len; i++) {
+            for(int j = 0; j < len; j++) {
+                System.out.print(matrix[i][j]+"\t");
+            }
+            System.out.println("");
+        }
+    }
+}
