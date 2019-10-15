578af60e67183fb9d0b8a3fdc0cfa1ec78289a5b
chong <chd830@naver.com>
2019-10-15 13:25:20 +0900

    GroupAnagram

--- src/com/code/Main.java
+++ src/com/code/Main.java
@@ -3,8 +3,15 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        RotateImage r = new RotateImage();
-        int[][] matrix = {{1,2,3},{4,5,6},{7,8,9}};
-        r.rotate(matrix);
+        GroupAnagrams g = new GroupAnagrams();
+        String[] strs = {"sat","lea","arm","sin","the","nod","guy","ins","rod"};
+        List<List<String>> result = g.groupAnagrams(strs);
+
+        for(List<String> l : result ){
+            for(int i = 0; i < l.size(); i++) {
+                System.out.print(l.get(i)+"\t");
+            }
+            System.out.println("");
+        }
     }
 }
