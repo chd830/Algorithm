b60db19fa9f60eff7f43678bff1c6009d3f4120a
chong <chd830@naver.com>
2019-10-15 17:51:43 +0900

    JumpGame

--- src/com/code/Main.java
+++ src/com/code/Main.java
@@ -3,11 +3,8 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        SpiralMatrix s = new SpiralMatrix();
-        int[][] matrix = {{1,2,3},{4,5,6},{7,8,9}};
-        List<Integer> list = s.spiralOrder(matrix);
-        for(int i : list) {
-            System.out.print(i+"\t");
-        }
+        JumpGame j = new JumpGame();
+        int[] nums = {3, 2, 1, 0, 4};
+        System.out.println(j.canJump(nums));
     }
 }
