fd18fa90423be244b6aeed0c9e0e72b53c6e15d4
chong <chd830@naver.com>
2019-10-16 17:08:28 +0900

    MergeIntervals

--- src/com/code/Main.java
+++ src/com/code/Main.java
@@ -4,8 +4,9 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         MergeIntervals m = new MergeIntervals();
-        int[][] intervals = {{1,3},{2,6},{8,10},{15,18}};
+        int[][] intervals = {{1,3},{8,10},{2,6},{15,18}};
         int[][] result = m.merge(intervals);
+        System.out.println("result");
         for(int i = 0; i < result.length; i++) {
             for(int j = 0; j < result[0].length; j++) {
                 System.out.print(result[i][j]+"\t");
--- src/com/code/MergeIntervals.java
+++ src/com/code/MergeIntervals.java
@@ -1,22 +1,32 @@
 package com.code;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class MergeIntervals {
     public int[][] merge(int[][] intervals) {
+
+        if(intervals.length <= 1) {
+            return intervals;
+        }
+        Arrays.sort(intervals, (i1, i2) -> Integer.compare(i1[0], i2[0]));
+
         List<int[]> list = new ArrayList();
-
-        int[] arr = new int[2];
-        for(int i = 0; i < intervals.length; i++) {
-
+        int[] arr = intervals[0];
+        list.add(arr);
+        for (int[] interval : intervals) {
+            System.out.println(interval[0]+", "+interval[1]);
         }
-
-        int[][] result = new int[2][list.size()];
-        for(int i = 0; i < list.size(); i++) {
-            result[i][0] = list.get(i)[0];
-            result[i][1] = list.get(i)[1];
+        for (int[] interval : intervals) {
+            if (interval[0] <= arr[1]) // Overlapping intervals, move the end if needed
+                arr[1] = Math.max(arr[1], interval[1]);
+            else {                             // Disjoint intervals, add the new interval to the list
+                arr = interval;
+                list.add(arr);
+            }
+            System.out.println(arr[0]+", "+arr[1]);
         }
-        return result;
+        return list.toArray(new int[list.size()][]);
     }
 }
