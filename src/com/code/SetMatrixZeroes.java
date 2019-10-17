786655b3b0823af74f553d952438a938bc589172
chong <chd830@naver.com>
2019-10-17 14:28:38 +0900

    Set Matrix Zeroes

--- src/com/code/SetMatrixZeroes.java
+++ src/com/code/SetMatrixZeroes.java
@@ -5,6 +5,9 @@ public class SetMatrixZeroes {
         boolean[][] visit = new boolean[matrix.length][matrix[0].length];
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[0].length; j++) {
+                if(matrix[i][j] == 0) {
+                    visit[i][j] = true;
+                }
                 System.out.print(matrix[i][j] + "\t");
             }
             System.out.println();
@@ -12,7 +15,7 @@ public class SetMatrixZeroes {
         System.out.println("-------------------------");
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[0].length; j++) {
-                if (visit[i][j] == false && matrix[i][j] == 0) {
+                if (visit[i][j] == true) {
                     check(visit, matrix, i, j);
                     System.out.println("-------------------------");
                 }
@@ -28,17 +31,9 @@ public class SetMatrixZeroes {
 
     public void check(boolean[][] visit, int[][] matrix, int i, int j) {
         for (int a = 0; a < matrix[0].length; a++) {
-            if(matrix[i][a] == 0)
-                visit[i][a] = false;
-            else
-                visit[i][a] = true;
             matrix[i][a] = 0;
         }
         for (int a = 0; a < matrix.length; a++) {
-            if(matrix[a][j] == 0)
-                visit[a][j] = false;
-            else
-                visit[a][j] = true;
             matrix[a][j] = 0;
         }
 
