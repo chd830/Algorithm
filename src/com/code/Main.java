928d7852de5e636f56c92dae0c864be765fc7092
chong <chd830@naver.com>
2019-10-20 17:08:31 +0900

    WordSearch

--- src/com/code/Main.java
+++ src/com/code/Main.java
@@ -3,9 +3,13 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Combinations c = new Combinations();
-        List<List<Integer>> result = c.combine(4,2);
-
+        WordSearch w = new WordSearch();
+        char[][] board = {{'A'}};
+                /*
+                [["A","B","C","E"],["S","F","E","S"],["A","D","E","E"]]
+"ABCESEEEFS"
+                 */
+        System.out.println(w.exist(board, "AB"));
 
     }
 }
