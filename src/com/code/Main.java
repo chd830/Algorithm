a10c664c546005f2b3f7782945267ab2e560a0b6
chong <chd830@naver.com>
2019-10-23 21:04:38 +0900

    SubsetsII

--- src/com/code/Main.java
+++ src/com/code/Main.java
@@ -3,13 +3,14 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        WordSearch w = new WordSearch();
-        char[][] board = {{'A'}};
-                /*
-                [["A","B","C","E"],["S","F","E","S"],["A","D","E","E"]]
-"ABCESEEEFS"
-                 */
-        System.out.println(w.exist(board, "AB"));
-
+        SubsetsII s = new SubsetsII();
+        int[] nums = {4,4,4,1,4};
+        List<List<Integer>> result = s.subsetsWithDup(nums);
+        for(List<Integer> l : result) {
+            for(int i = 0; i < l.size(); i++) {
+                System.out.print(l.get(i)+"\t");
+            }
+            System.out.println();
+        }
     }
 }
