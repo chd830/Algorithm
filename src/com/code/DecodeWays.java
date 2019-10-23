3f2cb292f69f0551e1ecd3462fa51eb77908772d
chong <chd830@naver.com>
2019-10-23 22:19:45 +0900

    DecodeWays접근방법만 세움

---
+++ src/com/code/DecodeWays.java
@@ -0,0 +1,29 @@
+package com.code;
+import java.util.*;
+
+public class DecodeWays {
+    public int numDecodings(String s) {
+        /*
+        s를 26이하의 수가 n번 나오게 나누기.
+
+         */
+//        int[] alph = new int[27];
+//        List<String> sub = new ArrayList();
+//
+//        for(int i = 0; i < s.length() - 1; i++) {
+//            for(int j = i; j < s.length(); j++) {
+//                String substring = s.substring(i, j+1);
+//                System.out.print(substring+"\t");
+//                if(!sub.contains(substring) && Integer.parseInt(substring) <= 26) {
+//                    sub.add(substring);
+//                }
+//            }
+//            System.out.println();
+//        }
+//        System.out.println("--------");
+//        for(String str : sub) {
+//            System.out.println(str+"\t");
+//        }
+        return 0;
+    }
+}
--- src/com/code/Main.java
+++ src/com/code/Main.java
@@ -3,14 +3,7 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        SubsetsII s = new SubsetsII();
-        int[] nums = {4,4,4,1,4};
-        List<List<Integer>> result = s.subsetsWithDup(nums);
-        for(List<Integer> l : result) {
-            for(int i = 0; i < l.size(); i++) {
-                System.out.print(l.get(i)+"\t");
-            }
-            System.out.println();
-        }
+        DecodeWays d  = new DecodeWays();
+        d.numDecodings("226");
     }
 }
