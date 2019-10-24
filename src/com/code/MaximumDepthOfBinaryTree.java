b5ec0f69d0e8e66af75921a7780cf30d3e84a2c1
chong <chd830@naver.com>
2019-10-24 15:26:51 +0900

    MaximumDepthOfBinaryTree

---
+++ src/com/code/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,17 @@
+package com.code;
+
+public class MaximumDepthOfBinaryTree {
+    public int maxDepth(TreeNode root) {
+        System.out.println("left: "+count(root.left, 0));
+        System.out.println("right: "+count(root.right, 0));
+        return Math.max(count(root.left,  0), count(root.right, 0)) + 1 ;
+    }
+
+    public int count(TreeNode t, int count) {
+        if (t == null)
+            return count;
+        else {
+            return Math.max(count(t.left, count + 1), count(t.right, count + 1));
+        }
+    }
+}
