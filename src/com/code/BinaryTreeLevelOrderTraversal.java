0648652b88accc524cdd97ada8c5585ba6a4e422
chong <chd830@naver.com>
2019-10-24 15:22:02 +0900

    BinaryTreeLevelOrderTraversal Solution

---
+++ src/com/code/BinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,22 @@
+package com.code;
+import java.util.*;
+
+public class BinaryTreeLevelOrderTraversal {
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList();
+
+        return result;
+    }
+
+    public void recur(List<List<Integer>> list, TreeNode root, int height) {
+        if(root == null)
+            return;
+        if(height >= list.size()) {
+            list.add(new ArrayList());
+        }
+        list.get(height).add(root.val);
+        recur(list, root.left, height + 1);
+        recur(list, root.right, height + 1);
+    }
+}
--- src/com/code/SameTree.java
+++ src/com/code/SameTree.java
@@ -3,15 +3,7 @@ package com.code;
 
 
 public class SameTree {
-    public class TreeNode {
-        int val;
-        TreeNode left;
-        TreeNode right;
 
-        TreeNode(int x) {
-            val = x;
-        }
-    }
     public boolean isSameTree(TreeNode p, TreeNode q) {
         if(p == null && q == null)
             return true;
