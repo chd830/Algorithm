210fc08b5de4fffaa0040b8c761bc933af9d6cc1
chong <chd830@naver.com>
2019-10-29 16:55:56 +0900

    CountingBits

--- src/com/code/Main.java
+++ src/com/code/Main.java
@@ -3,11 +3,9 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        LongestIncreasingSubsequence l = new LongestIncreasingSubsequence();
-        System.out.println(l.lengthOfLIS(new int[] {4,10,4,3,8,9}));
-        System.out.println("---------");
-        System.out.println(l.lengthOfLIS(new int[] {10,9,2,5,3,4}));
-        System.out.println("---------");
-        System.out.println(l.lengthOfLIS(new int[] {-2, -1}));
+        CountingBits c = new CountingBits();
+        int[] result = c.countBits(32);
+        for(int i : result)
+            System.out.print(i+"\t");
     }
 }
