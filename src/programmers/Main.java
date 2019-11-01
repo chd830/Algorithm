8e16a475f26171b69846ddd05d79f4362149035a
chong <chd830@naver.com>
2019-11-01 17:11:20 +0900

    프로그래머스 2016

--- src/programmers/Main.java
+++ src/programmers/Main.java
@@ -2,6 +2,7 @@ package programmers;
 
 public class Main {
     public static void main(String[] args) {
-
+        DayOfWeek d = new DayOfWeek();
+        System.out.println(d.solution(5,24));
     }
 }
