3a3625d5b12126069e916aa90ec402f05f3173c8
chong <chd830@naver.com>
2020-02-07 23:49:16 +0900

    색종이 붙이기, 달팽이 숫자 재귀

--- src/com/swexpertacademy/d2_1954.java
+++ src/com/swexpertacademy/d2_1954.java
@@ -5,88 +5,118 @@ import java.util.Scanner;
 
 //달팽이 숫자
 public class d2_1954 {
-    public d2_1954() {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int T = sc.nextInt();
-        for(int t = 1; t <= T; t++) {
-            int num = sc.nextInt();
-            int[][] arr = new int[num][num];
-
-            int count = 1;
-            int r = 0;
-            int c = 0;
-//            int i = 1;
-            int index = 0;
-            for(int j = num; j >= 0; j -= 2) { //j는 num에서 -2만큼 지속적으로 줄어든다.
-                for(r = 0; r < j; r++) {//오른쪽으로 감
-                    arr[index][index+r] = count++;
-                }
-                for(c = 1; c < j; c++) {//아래로 내려감
-                    //num-index-1은 맨바깥에서 안쪽으로 한칸씩 이동하게 한다.
-                    arr[index+c][num-index-1] = count++;
-                }
-                for(r = 1; r < j; r++) {//왼쪽으로 감
-                    //num-index-r-1은 맨바깥에서 이미 지나온 r만큼 뺀다.
-                    arr[num-index-1][num-index-r-1] = count++;
-                }
-                for(c = 1; c < j - 1; c++) {//위로 감
-                    //num-indx-c-1도 역시 맨바깥에서 지나온 c만큼 뺀다.
-                    arr[num-index-c-1][index] = count++;
-                }
-                index++;
+        for (int t = 1; t <= T; t++) {
+            N = sc.nextInt();
+            arr = new int[N][N];
+            recur(0, 0, 1);
+            for(int i = 0; i < N; i++) {
+                System.out.println(Arrays.toString(arr[i]));
             }
-            System.out.println("#"+t);
-            print(arr);
         }
     }
-    static void print(int[][] arr) {
-        for(int l = 0; l < arr.length; l++) {
-            System.out.println(Arrays.toString(arr[l]));
+
+    static int[][] arr;
+    static int N;
+    public static void recur(int r, int c, int count) {
+        if(count == N * N+1) {
+            return;
         }
+        arr[r][c] = count;
+        if(c < N - 1 && arr[r][c+1] == 0)
+            recur(r, c+1, count+1);
+        if(r < N - 1 && arr[r+1][c] == 0)
+            recur(r+1, c, count+1);
+        if(0 < c && arr[r][c-1] == 0)
+            recur(r, c-1, count+1);
+        if(0 < r && arr[r-1][c] == 0)
+            recur(r-1, c, count+1);
     }
-//    public d2_1954() {
+}
+//    public static void main(String[] args) {
 //        Scanner sc = new Scanner(System.in);
 //        int T = sc.nextInt();
-//        for (int t = 1; t <= T; t++) {
-//            int N = sc.nextInt();
-//            int[][] arr = new int[N][N];
-//            System.out.println("#" + t);
-//            int x = 0;
-//            int y = -1;
-//            int k = 1;
-//            int l = N;
-//            int count = 1;
-//            while (true) {
-//                if(count > N*N) {
-//                    break;
-//                }
-//                for (int n = 0; n < l; n++) {
-//                    y = y + k;
-////                    System.out.println("y: "+y);
-//                    arr[x][y] = count;
-//                    count++;
-//                }
-//                l--;
+//        for(int t = 1; t <= T; t++) {
+//            int num = sc.nextInt();
+//            int[][] arr = new int[num][num];
 //
-//                for (int n = 0; n < l; n++) {
-//                    x = x + k;
-////                    System.out.println("x: "+x);
-//                    arr[x][y] = count;
-//                    count++;
+//            int count = 1;
+//            int r = 0;
+//            int c = 0;
+////            int i = 1;
+//            int index = 0;
+//            for(int j = num; j >= 0; j -= 2) { //j는 num에서 -2만큼 지속적으로 줄어든다.
+//                for(r = 0; r < j; r++) {//오른쪽으로 감
+//                    arr[index][index+r] = count++;
 //                }
-//                k = -k ;
+//                for(c = 1; c < j; c++) {//아래로 내려감
+//                    //num-index-1은 맨바깥에서 안쪽으로 한칸씩 이동하게 한다.
+//                    arr[index+c][num-index-1] = count++;
+//                }
+//                for(r = 1; r < j; r++) {//왼쪽으로 감
+//                    //num-index-r-1은 맨바깥에서 이미 지나온 r만큼 뺀다.
+//                    arr[num-index-1][num-index-r-1] = count++;
+//                }
+//                for(c = 1; c < j - 1; c++) {//위로 감
+//                    //num-indx-c-1도 역시 맨바깥에서 지나온 c만큼 뺀다.
+//                    arr[num-index-c-1][index] = count++;
+//                }
+//                index++;
 //            }
+//            System.out.println("#"+t);
 //            print(arr);
 //        }
 //    }
-
 //    static void print(int[][] arr) {
-//        for (int i = 0; i < arr.length; i++) {
-//            for (int j = 0; j < arr.length; j++) {
-//                System.out.print(arr[i][j] + " ");
-//            }
-//            System.out.println();
+//        for(int l = 0; l < arr.length; l++) {
+//            System.out.println(Arrays.toString(arr[l]));
 //        }
 //    }
-
-}
+////    public d2_1954() {
+////        Scanner sc = new Scanner(System.in);
+////        int T = sc.nextInt();
+////        for (int t = 1; t <= T; t++) {
+////            int N = sc.nextInt();
+////            int[][] arr = new int[N][N];
+////            System.out.println("#" + t);
+////            int x = 0;
+////            int y = -1;
+////            int k = 1;
+////            int l = N;
+////            int count = 1;
+////            while (true) {
+////                if(count > N*N) {
+////                    break;
+////                }
+////                for (int n = 0; n < l; n++) {
+////                    y = y + k;
+//////                    System.out.println("y: "+y);
+////                    arr[x][y] = count;
+////                    count++;
+////                }
+////                l--;
+////
+////                for (int n = 0; n < l; n++) {
+////                    x = x + k;
+//////                    System.out.println("x: "+x);
+////                    arr[x][y] = count;
+////                    count++;
+////                }
+////                k = -k ;
+////            }
+////            print(arr);
+////        }
+////    }
+//
+////    static void print(int[][] arr) {
+////        for (int i = 0; i < arr.length; i++) {
+////            for (int j = 0; j < arr.length; j++) {
+////                System.out.print(arr[i][j] + " ");
+////            }
+////            System.out.println();
+////        }
+////    }
+//
+//}
--- src/net/acmicpc/색종이붙이기.java
+++ src/net/acmicpc/색종이붙이기.java
@@ -1,67 +1,130 @@
 package net.acmicpc;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class 색종이붙이기 {
-    static int[][] map = new int[10][10];
-    static int[] paper = {0, 5, 5, 5, 5, 5};
+    static int[][] arr;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        arr = new int[10][10];
+        int[] paper = new int[5];
         for(int i = 0; i < 10; i++) {
-            for(int j = 0; j < 10; j++)
-                map[i][j] = sc.nextInt();
-        }
-        dfs(0);
-        System.out.println(ans == 987654321 ? -1 : ans);
-    }
-    static void dfs(int cnt) {
-        int startR = -1;
-        int startC = -1;
-        out:for(int i = 0; i < 10; i++) {
             for(int j = 0; j < 10; j++) {
-                if( map[i][j] == 1 ) {
-                    startR = i;
-                    startC = j;
-                    break out;
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        for(int a = 5; a > 0; a--) {
+            for (int i = 0; i < 10; i++) {
+                for (int j = 0; j < 10; j++) {
+                    if (arr[i][j] == 1) {
+                        if(check(a, i, j)) {
+                            paper[a-1]++;
+                            paste(a, i, j);
+//                            for(int k = 0; k < 10; k++) {
+//                                System.out.println(Arrays.toString(arr[k]));
+//                            }
+//                            System.out.println(Arrays.toString(paper));
+                        }
+                    }
                 }
             }
         }
-        if( startR == -1 && startC == -1 ) {
-            ans = Math.min(ans, cnt);
+        int sum = 0;
+        for(int p : paper) {
+            if(p > 5) {
+                System.out.println("-1");
+                System.exit(0);
+            }
+            else {
+                sum += p;
+            }
+        }
+        System.out.println(sum);
+    }
+    public static boolean check(int size, int x, int y) {
+        if(x + size > arr.length || y + size > arr.length)
+            return false;
+//        System.out.println("("+x+", "+y+")의 "+size+"색종이");
+        for(int i = x; i < x + size; i++) {
+            for(int j = y; j < y + size; j++) {
+                if(arr[i][j] == 0) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static void paste(int size, int x, int y) {
+        if(x + size > arr.length || y + size > arr.length)
             return;
-        }
-        int max = 5;
-        while( max > 0 ) {
-            boolean isOk = true;
-            out: for(int i = 0; i < max; i++) {
-                for(int j = 0; j < max; j++) {
-                    if( startR + i >= 10 || startC + j >= 10 || map[startR+i][startC+j] == 0 ) {
-                        isOk = false;
-                        break out;
-                    }
-                }
-            }
-            if(isOk)
-                break;
-            max--;
-        }
-        for(int i = max; i > 0; i--) {
-            if( paper[i] > 0) {
-                for(int r = startR; r < startR + i; r++) {
-                    for(int c = startC; c < startC + i; c++) {
-                        map[r][c] = 0;
-                    }
-                }
-                paper[i]--;
-                dfs(cnt+1);
-                paper[i]++;
-                for(int r = startR; r < startR + i; r++) {
-                    for(int c = startC; c < startC + i; c++) {
-                        map[r][c] = 1;
-                    }
-                }
+        for(int i = x; i < x + size; i++) {
+            for(int j = y; j < y + size; j++) {
+                arr[i][j] = 0;
             }
         }
     }
-    static int ans = 987654321;
+
+//    static int[][] map = new int[10][10];
+//    static int[] paper = {0, 5, 5, 5, 5, 5};
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        for(int i = 0; i < 10; i++) {
+//            for(int j = 0; j < 10; j++)
+//                map[i][j] = sc.nextInt();
+//        }
+//        dfs(0);
+//        System.out.println(ans == 987654321 ? -1 : ans);
+//    }
+//    static void dfs(int cnt) {
+//        int startR = -1;
+//        int startC = -1;
+//        out:for(int i = 0; i < 10; i++) {
+//            for(int j = 0; j < 10; j++) {
+//                if( map[i][j] == 1 ) {
+//                    startR = i;
+//                    startC = j;
+//                    break out;
+//                }
+//            }
+//        }
+//        if( startR == -1 && startC == -1 ) {
+//            ans = Math.min(ans, cnt);
+//            return;
+//        }
+//        int max = 5;
+//        while( max > 0 ) {
+//            boolean isOk = true;
+//            out: for(int i = 0; i < max; i++) {
+//                for(int j = 0; j < max; j++) {
+//                    if( startR + i >= 10 || startC + j >= 10 || map[startR+i][startC+j] == 0 ) {
+//                        isOk = false;
+//                        break out;
+//                    }
+//                }
+//            }
+//            if(isOk)
+//                break;
+//            max--;
+//        }
+//        for(int i = max; i > 0; i--) {
+//            if( paper[i] > 0) {
+//                for(int r = startR; r < startR + i; r++) {
+//                    for(int c = startC; c < startC + i; c++) {
+//                        map[r][c] = 0;
+//                    }
+//                }
+//                paper[i]--;
+//                dfs(cnt+1);
+//                paper[i]++;
+//                for(int r = startR; r < startR + i; r++) {
+//                    for(int c = startC; c < startC + i; c++) {
+//                        map[r][c] = 1;
+//                    }
+//                }
+//            }
+//        }
+//    }
+//    static int ans = 987654321;
 }
