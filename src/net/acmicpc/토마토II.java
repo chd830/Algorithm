b91fe01ceab0484dd09cff0234dd590c12038a30
chong <chd830@naver.com>
2020-02-15 23:42:44 +0900

    효율적인해킹

---
+++ src/net/acmicpc/토마토II.java
@@ -0,0 +1,85 @@
+package net.acmicpc;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class 토마토II {
+    static int N;
+    static int M;
+    static int H;
+    static int[][][] box;
+    static int[][] pos = {{1, 0, 0},{-1, 0, 0}, {0, 1, 0}, {0, 0, 1}, {0, 0, -1}, {0, -1, 0}};
+    static Queue<Node> que;
+
+    static class Node {
+        int z;
+        int x;
+        int y;
+        Node(int z, int x, int y) {
+            this.z = z;
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+        H = sc.nextInt();
+        box = new int[H][M][N];
+        que = new LinkedList<>();
+        for(int i = 0; i < H; i++) {
+            for(int j = 0; j < M; j++) {
+                for(int k = 0; k < N; k++) {
+                    box[i][j][k] = sc.nextInt();
+                    if(box[i][j][k] == 1) {
+                        que.add(new Node(i, j, k));
+                    }
+                }
+            }
+        }
+//        print();
+        while(!que.isEmpty()) {
+            Node n = que.poll();
+            for(int l = 0; l < 6; l++) {
+                int dh = n.z + pos[l][0];
+                int dx = n.x + pos[l][1];
+                int dy = n.y + pos[l][2];
+                if(dh < H && dx < M && dy < N && dh >= 0 && dx >= 0 && dy >= 0 && box[dh][dx][dy] != -1 && box[dh][dx][dy] == 0) {
+                    box[dh][dx][dy] = box[n.z][n.x][n.y] + 1;
+                    que.add(new Node(dh, dx, dy));
+                }
+            }
+        }
+        print();
+        int max = 0;
+        for(int i = 0; i < H; i++) {
+            for(int j = 0; j < M; j++) {
+                for (int k = 0; k < N; k++) {
+                    if(box[i][j][k] == 0) {
+                        System.out.println("-1");
+                        System.exit(0);
+                    }
+                    if(max < box[i][j][k]) {
+                        max = box[i][j][k];
+                    }
+                }
+            }
+        }
+        System.out.println(--max);
+    }
+    public static void print() {
+        for(int i = 0; i < H; i++) {
+            for(int j = 0; j < M; j++) {
+                for(int k = 0; k < N; k++) {
+                    System.out.print(box[i][j][k]+" ");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+}
--- src/net/acmicpc/효율적인해킹.java
+++ src/net/acmicpc/효율적인해킹.java
@@ -1,36 +1,79 @@
 package net.acmicpc;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class 효율적인해킹 {
     static int N;
     static int M;
     static int cnt;
-    static int[] count;
+    static boolean[] visited;
     static List<Integer>[] list;
-    static boolean[] check;
-
+    static int[] check;
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//        Scanner sc = new Scanner(System.in);
-        StringTokenizer token = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(token.nextToken());
-        int[] arr = new int[N+1];
-        M = Integer.parseInt(token.nextToken());
-        list = new ArrayList[N+1];
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+        N = sc.nextInt();
+        M = sc.nextInt();
+        list = new ArrayList[100001];
+        check = new int[100001];
         for(int i = 0; i <= N; i++) {
             list[i] = new ArrayList();
         }
         for(int i = 0; i < M; i++) {
-            token = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(token.nextToken());
-            int b = Integer.parseInt(token.nextToken());
+            int a = sc.nextInt();
+            int b = sc.nextInt();
             list[a].add(b);
-            list[b].add(a);
-
         }
+        for(int i = 1; i <= N; i++) {
+            if(list[i].size() != 0) {
+                visited = new boolean[100001];
+                dfs(i);
+//                bfs(i);
+//                Arrays.fill(visited, false);
+            }
+        }
+        int max = 0;
+        for(int i = 1; i <= N; i++) {
+            if(max < check[i]) {
+                max = check[i];
+            }
+        }
+        for(int i = 1; i <= N; i++) {
+            if(check[i] == max) {
+                sb.append(
+                        i+" ");
+            }
+        }
+        System.out.println(sb.toString());
+    }
+    public static void dfs(int start) {
+        visited[start] = true;
+        for(int l : list[start]) {
+            if(!visited[l]) {
+                check[l]++;
+                dfs(l);
+            }
+        }
+    }
+    public static void bfs(int start) {
+        Queue<Integer> que = new LinkedList<>();
+        que.add(start);
+        while(!que.isEmpty()) {
+            int num = que.poll();
+            for(int n : list[num]) {
+                if(!visited[n]) {
+                    que.add(n);
+                    visited[n] = true;
+                }
+            }
+        }
+        for(int i = 1; i <= N; i++) {
+            if(visited[i] == true) {
+                cnt++;
+            }
+        }
+        check[start] = cnt;
+        cnt = 0;
     }
 }
