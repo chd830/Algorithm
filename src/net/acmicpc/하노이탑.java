adcd6c7339ea6e08fa52ca87774b2220fc847780
chong <chd830@naver.com>
2020-03-31 22:10:41 +0900

    하노이탑

---
+++ src/net/acmicpc/하노이탑.java
@@ -0,0 +1,31 @@
+package net.acmicpc;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 하노이탑 {
+    static int cnt = 0;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        hanoi(N, 1, 2, 3);
+        System.out.println(cnt);
+        System.out.println(sb);
+    }
+    static void hanoi(int N, int from, int by, int to) {
+        cnt++;
+        if(N == 1) {
+            sb.append(from).append(" ").append(to).append("\n");
+        }
+        else {
+            //마지막 탑을 보조탑으로 사용하여 첫번째탑의 n-1개의 원반을 중간으로 이동
+            hanoi(N-1, from, to, by);
+            sb.append(from).append(" ").append(to).append("\n");
+            //첫번째 탑을 보조탑으로 사용하여 중간탑의 n-1개의 원반을 마지막탑으로 이동
+            hanoi(N-1, by, from, to);
+        }
+    }
+}
+
