93b78c5c2112bb233d906b3d00935c78f0ca6fac
chong <chd830@naver.com>
2020-04-07 22:31:05 +0900

    별찍기 10

--- src/net/acmicpc/별찍기10.java
+++ src/net/acmicpc/별찍기10.java
@@ -1,41 +1,38 @@
 package net.acmicpc;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class 별찍기10 {
     private static final Scanner sc = new Scanner(System.in);
     private static char[][] arr;
+    static int N;
 
     public static void main(String args[]) {
-        int num = sc.nextInt();
-        arr = new char[num][num];
-
-        for (int i = 0; i < num; i++) {
-            Arrays.fill(arr[i], ' ');
+        N = sc.nextInt();
+        arr = new char[N][N];
+        recur(0, 0, N);
+        for(int i = 0; i < N; i++) {
+            for(int j = 0;j < N; j++) {
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
         }
-
-        recur(0, 0, num);
-//        for (int i = 0; i < num; i++) {
-//            System.out.println(visited1[i]);
-//        }
     }
-
-    public static void recur (int x, int y, int n) {
-        if(n < 1) {
+    static void recur(int x, int y, int num) {
+        int div = 0;
+        if(num == 1) {
+            arr[x][y] = '*';
             return;
         }
-        arr[x][y] = '*';
-        for(int i = 0; i < arr.length; i++) {
-            System.out.println(Arrays.toString(arr[i]));
+        div = num/3;
+        for(int i = 0; i < 3; i++) {
+            for(int j = 0; j < 3; j++) {
+                if(i == 1 && j == 1) {
+                    continue;
+                }
+                recur(x+(div*i), y+(div*j), div);
+            }
         }
-        System.out.println();
-        recur(x, y, n/3);
-        recur(x, y+n/3, n/3);
-        recur(x, y+n/3*2, n/3);
-        recur(x+n/3, y, n/3);
-        recur(x+n/3, y+n/3*2, n/3);
-        recur(x+n/3*2, y, n/3);
-        recur(x+n/3*2, y+n/3, n/3);
-        recur(x+n/3*2, y+n/3*2, n/3);
     }
 }
