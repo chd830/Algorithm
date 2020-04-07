c8611374f24f911f6f7ed5e3c35dc11ee498976b
chong <chd830@naver.com>
2020-04-07 21:56:27 +0900

    Z

--- src/net/acmicpc/Z.java
+++ src/net/acmicpc/Z.java
@@ -3,43 +3,30 @@ package net.acmicpc;
 import java.util.Scanner;
 
 public class Z {
+    static int cnt;
     static int xPos;
     static int yPos;
-    static int[][] map;
-    static int[][] cnt;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
+        cnt = 0;
         xPos = sc.nextInt()+1;
         yPos = sc.nextInt()+1;
-        int pow = (int)Math.pow(2, N);
-        map = new int[pow+1][pow+1];
-        cnt = new int[pow+1][pow+1];
-        find(1, 1, 1);
+        find(1, 1, (int)Math.pow(2, N));
     }
-    static void find(int x, int y, int cnt) {
-        System.out.println("("+x+", "+y+"): "+cnt);
-        if(x == xPos && y == yPos) {
-            System.out.println(cnt-1);
-            System.exit(0);
+    static void find(int x, int y, int size) {
+        System.out.println("("+x+", "+y+"): "+size);
+        if(size == 1) {
+            if(x == xPos && y == yPos) {
+                System.out.println(cnt);
+            }
+            cnt++;
+            return;
         }
-        if(cnt%32 == 0) {
-            find(x+1, y-7, cnt+1);
-        }
-        else if(cnt%16 == 0) {
-            find(x-3, y+1, cnt+1);
-        }
-        else if(cnt%8 == 0) {
-            find(x+1, y-3, cnt+1);
-        }
-        else if(cnt%4 == 0) {
-            find(x-1, y+1, cnt+1);
-        }
-        else if(cnt%2 == 0) {
-            find(x+1, y-1, cnt+1);
-        }
-        else {
-            find(x, y+1, cnt+1);
-        }
+        int s = size/2;
+        find(x, y, s);
+        find(x, y+s, s);
+        find(x+s, y, s);
+        find(x+s, y+s, s);
     }
 }
