22c0fed44bf57e1ae1f517b76a9b062028c8d645
chong <chd830@naver.com>
2020-06-05 22:43:27 +0900

    괄호의 값

---
+++ src/review/괄호의값2504.java
@@ -0,0 +1,95 @@
+package review;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class 괄호의값2504 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+
+        Stack<String> stack = new Stack<String>();
+
+        boolean isAble = true;
+        for(char ch : str.toCharArray()) {
+            String c = ""+ch;
+            if("(".equals(c)) {
+                stack.push(")");
+                continue;
+            }
+
+            if("[".equals(c)) {
+                stack.push("]");
+                continue;
+            }
+
+            int num = 0;
+            while(true) {
+                if(stack.isEmpty()) {
+                    isAble = false;
+                    break;
+                }
+
+                if(isNumber(stack.peek())) {
+                    num += Integer.parseInt(stack.pop());
+                }
+                else {
+                    //스택에서 짝이 맞지 않거나 숫자가 아니라면 isAble = false
+                    //짝이 맞는다면 숫자를 더한다.
+                    if(isVPS(c, stack.peek())) {
+                        stack.pop();
+                        int t = (")".equals(c)) ? 2:3;
+
+                        if(num == 0) {
+                            stack.push(String.valueOf(t));
+                        }
+                        else {
+                            stack.push(String.valueOf(t*num));
+                        }
+                        break;
+                    }
+                    else {
+                        isAble = false;
+                        break;
+                    }
+                }
+            }
+            if(!isAble) {
+                break;
+            }
+        }
+
+        int result = 0;
+
+        while(!stack.isEmpty()) {
+            if(isNumber(stack.peek())) {
+                result += Integer.parseInt(stack.pop());
+            }
+            else {
+                isAble = false;
+                break;
+            }
+        }
+
+        if(isAble) System.out.println(result);
+        else System.out.println(0);
+    }
+
+    public static boolean isVPS(String c, String target) {
+        if(c.equals(target)) {
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean isNumber(String str) {
+        if(str.equals(")") || str.equals("]")) {
+            return false;
+        }
+        return true;
+    }
+}
+
---
+++ src/review/달이차오른다가자1194.java
@@ -0,0 +1,74 @@
+package review;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+//3차원배열 사용하기
+public class 달이차오른다가자1194 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer token = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(token.nextToken());
+        M = Integer.parseInt(token.nextToken());
+        map = new char[N][M];
+        Node start = null;
+        for(int i = 0; i < N; i++) {
+            String str = br.readLine();
+            for(int j = 0; j < M; j++) {
+                map[i][j] = str.charAt(j);
+                if(map[i][j] == '0') {
+                    start = new Node(i, j, 0);
+                }
+            }
+        }
+        Queue<Node> que = new LinkedList<>();
+        que.add(start);
+        while(!que.isEmpty()) {
+            int size = que.size();
+            Node n = que.poll();
+            for(int i = 0; i < size; i++) {
+                for(int d = 0; d < 4; d++) {
+                    int dx = n.x + dir[d][0];
+                    int dy = n.y + dir[d][1];
+                    if(dx < 0 || dy < 0 || dx >= N || dy >= M || map[dx][dy] == '#') {
+                        continue;
+                    }
+                    if(map[dx][dy] == '1') {
+                        System.out.println(n.cnt + 1);
+                        System.exit(0);
+                    }
+                    else if(map[dx][dy] == '.' || map[dx][dy] == '0') {
+                        que.add(new Node(dx, dy, n.cnt + 1));
+                    }
+                    else if(map[dx][dy] >= 'a' && map[dx][dy] <= 'z') {
+                        alph[map[dx][dy]-97]++;
+                        que.add(new Node(dx, dy, n.cnt+1));
+                    }
+                    else if(map[dx][dy] >= 'A' && map[dx][dy] <= 'Z' && alph[map[dx][dy]-65] != 0) {
+                        que.add(new Node(dx, dy, n.cnt+1));
+                    }
+                }
+            }
+        }
+        System.out.println(-1);
+    }
+    static int N;
+    static int M;
+    static char[][] map;
+    static int[] alph = new int[26];
+    static int[][] dir = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+static class Node {
+    int x;
+    int y;
+    int cnt;
+    Node(int x, int y, int cnt) {
+        this.x = x;
+        this.y = y;
+        this.cnt = cnt;
+    }
+}
+}
