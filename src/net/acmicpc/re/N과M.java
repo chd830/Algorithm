f05a2c55faaf139725e0f36f381e16553a0f95be
eunchong <chd830@naver.com>
2020-09-24 23:46:14 +0900

    N과M

---
+++ src/net/acmicpc/re/N과M.java
@@ -0,0 +1,41 @@
+package net.acmicpc.re;
+
+import java.util.*;
+import java.io.*;
+
+public class N과M {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer token = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(token.nextToken());
+        M = Integer.parseInt(token.nextToken());
+
+        visited = new boolean[N];
+        arr = new int[M];
+        sequence(0);
+    }
+
+    static int N;
+    static int M;
+    static int[] arr;
+    static boolean[] visited;
+
+    public static void sequence(int idx) {
+        if(idx == M) {
+            for(int i = 0; i < M; i++) {
+                System.out.print(arr[i]+" ");
+            }
+            System.out.println();
+            return;
+        }
+        for(int i = 0; i < N; i++) {
+            if(!visited[i]) {
+                visited[i] = true;
+                arr[idx] = i+1;
+                sequence(idx+1);
+                visited[i] = false;
+            }
+        }
+    }
+}
