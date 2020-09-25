2df90369db733a8ff2e8f1630ad0a30a371c5b09
eunchong <chd830@naver.com>
2020-09-25 23:48:25 +0900

    주식가격

--- src/net/acmicpc/re/주식가격.java
+++ src/net/acmicpc/re/주식가격.java
@@ -1,5 +1,24 @@
 package net.acmicpc.re;
 
-public class 주식가격 {
+import java.util.*;
+import java.io.*;
 
+public class 주식가격 {
+    public static void main(String[] args) {
+        int[] result = solution(new int[] {1, 2, 3, 2, 3});
+        System.out.println(Arrays.toString(result));
+    }
+    public static int[] solution(int[] prices) {
+        int len = prices.length;
+        int[] result = new int[len];
+        for(int i = 0; i < len; i++ ){
+            for(int j = i+1; j < len; j++) {
+                result[i] = j-i;
+                if(prices[i] > prices[j]) {
+                    break;
+                }
+            }
+        }
+        return result;
+    }
 }
