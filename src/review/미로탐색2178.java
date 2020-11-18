ba6f71c377b5b3216b2ef11c964fadffcb2679c2
eunchong <chd830@naver.com>
2020-11-18 23:40:23 +0900

    미로탐색 re

--- src/review/미로탐색2178.java
+++ src/review/미로탐색2178.java
@@ -8,37 +8,69 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class 미로탐색2178 {
-    public static void main(String[] args) throws IOException {
+    public static int solution() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer token = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(token.nextToken());
         int M = Integer.parseInt(token.nextToken());
-        char[][] map = new char[N][M];
-        boolean[][] visited = new boolean[N][M];
+        int[][] map = new int[N][M];
         for(int i = 0; i < N; i++) {
             String str = br.readLine();
             for(int j = 0; j < M; j++) {
-                map[i][j] = str.charAt(j);
+                map[i][j] = str.charAt(j) - '0';
             }
         }
-        Queue<Node> que = new LinkedList();
+        Queue<Node> que = new LinkedList<>();
         que.add(new Node(0, 0, 1));
-        visited[0][0] = true;
+        boolean[][] visited = new boolean[N][M];
         while(!que.isEmpty()) {
             Node n = que.poll();
             if(n.x == N-1 && n.y == M-1) {
-                System.out.println(n.cnt);
-                break;
+                return n.cnt;
             }
             for(int d = 0; d < 4; d++) {
                 int dx = n.x + dir[d][0];
                 int dy = n.y + dir[d][1];
-                if(dx >= 0 && dy >= 0 && dx < N && dy < M && !visited[dx][dy] && map[dx][dy] == '1') {
+                if(dx >= 0 && dy >= 0 && dx < N && dy < M && !visited[dx][dy] && map[dx][dy] == 1) {
                     que.add(new Node(dx, dy, n.cnt+1));
                     visited[dx][dy] = true;
                 }
             }
         }
+        return 0;
+    }
+    public static void main(String[] args) throws IOException {
+        System.out.println(solution());
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer token = new StringTokenizer(br.readLine());
+//        int N = Integer.parseInt(token.nextToken());
+//        int M = Integer.parseInt(token.nextToken());
+//        char[][] map = new char[N][M];
+//        boolean[][] visited = new boolean[N][M];
+//        for(int i = 0; i < N; i++) {
+//            String str = br.readLine();
+//            for(int j = 0; j < M; j++) {
+//                map[i][j] = str.charAt(j);
+//            }
+//        }
+//        Queue<Node> que = new LinkedList();
+//        que.add(new Node(0, 0, 1));
+//        visited[0][0] = true;
+//        while(!que.isEmpty()) {
+//            Node n = que.poll();
+//            if(n.x == N-1 && n.y == M-1) {
+//                System.out.println(n.cnt);
+//                break;
+//            }
+//            for(int d = 0; d < 4; d++) {
+//                int dx = n.x + dir[d][0];
+//                int dy = n.y + dir[d][1];
+//                if(dx >= 0 && dy >= 0 && dx < N && dy < M && !visited[dx][dy] && map[dx][dy] == '1') {
+//                    que.add(new Node(dx, dy, n.cnt+1));
+//                    visited[dx][dy] = true;
+//                }
+//            }
+//        }
     }
     static int[][] dir = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
     static class Node {
