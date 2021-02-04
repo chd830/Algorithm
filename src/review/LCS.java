2f31d56dd28ca9d6533068d9c89306dfa9a4bd8f
EunChong <chd830@naver.com>
2021-02-04 15:50:57 +0900

    LCS

--- src/review/LCS.java
+++ src/review/LCS.java
@@ -8,17 +8,23 @@ public class LCS {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String s1 = br.readLine();
         String s2 = br.readLine();
-        int[][] strs = new int[s1.length()][s2.length()];
-        for(int i = 0; i < s1.length(); i++) {
-            int len = 0;
-            for(int j = 0; j < s2.length(); j++) {
-                if(s1.charAt(i) == s2.charAt(j)) {
+        int[][] strs = new int[s1.length()+1][s2.length()+1];
+
+        for(int i = 0; i < strs.length; i++) {
+            for(int j = 0; j < strs[0].length; j++) {
+                if(i == 0 || j == 0) {
+                    strs[i][j] = 0;
+                    continue;
+                }
+                if(s1.charAt(i-1) == s2.charAt(j-1)) {
+                    strs[i][j] = strs[i-1][j-1]+1;
+                }
+                else {
+                    strs[i][j] = Math.max(strs[i-1][j], strs[i][j-1]);
                 }
             }
         }
-        for(int i = 0; i < strs.length; i++) {
-            System.out.println(Arrays.toString(strs[i]));
-        }
+        System.out.println(strs[s1.length()][s2.length()]);
     }
 
 }
