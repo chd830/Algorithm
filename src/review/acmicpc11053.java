fc376831e30003886d207779cf506ca14d298c93
EunChong <chd830@naver.com>
2021-02-25 00:48:58 +0900

    가장 큰 증가 부분 수열

---
+++ src/review/acmicpc11053.java
@@ -0,0 +1,28 @@
+package review;
+
+import java.util.*;
+import java.io.*;
+
+public class acmicpc11053 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer token = new StringTokenizer(br.readLine());
+        int[] arr = new int[N];
+        for(int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(token.nextToken());
+        }
+        int[] d = new int[N];
+        Arrays.fill(d, 1);
+        int max = 1;
+        for(int i = 1; i < N; i++) {
+            for(int j = 0; j < i; j++) {
+                if(arr[i] > arr[j]) {
+                    d[i] = Math.max(d[i], d[j]+1);
+                }
+            }
+            max = Math.max(max, d[i]);
+        }
+        System.out.println(max);
+    }
+}
