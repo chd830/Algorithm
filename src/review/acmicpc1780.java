8adfffa6becc5d9be41ebb76f0ee7f5657270b73
EunChong <chd830@naver.com>
2021-03-18 15:53:08 +0900

    종이의 개수

--- src/review/acmicpc1780.java
+++ src/review/acmicpc1780.java
@@ -3,59 +3,61 @@ package review;
 import java.util.*;
 import java.io.*;
 
-// 색종이 만들기
-public class acmicpc2630 {
+// 종이의 개수
+public class acmicpc1780 {
     static int N;
     static int[][] arr;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
         arr = new int[N][N];
-        StringTokenizer token;
+        StringTokenizer token = null;
         for(int i = 0; i < N; i++) {
             token = new StringTokenizer(br.readLine());
             for(int j = 0; j < N; j++) {
                 arr[i][j] = Integer.parseInt(token.nextToken());
             }
         }
-        recur(0, 0, N);
+
+//        cut(N, 0, 0);
+//        System.out.println();
+        dfs(0, 0, N);
+        System.out.println(minus);
+        System.out.println(zero);
+        System.out.println(plus);
     }
+    static int zero;
+    static int plus;
+    static int minus;
     static boolean check(int x, int y, int size) {
         int first = arr[x][y];
         for(int i = x; i < x+size; i++) {
             for(int j = y; j < y+size; j++) {
-                if(arr[i][j] == first) {
+                if(arr[i][j] != first) {
                     return false;
                 }
             }
         }
         if(first == 0) {
-            white++;
+            zero++;
+        }
+        else if(first == 1) {
+            plus++;
         }
         else {
-            blue++;
+            minus++;
         }
         return true;
     }
-    static int blue = 0;
-    static int white = 0;
-    static void recur(int x, int y, int size) {
-        System.out.println(x+"\t"+y+"\t"+size);
-        if(size == 1) {
-            if(arr[x][y] == 1) {
-                blue++;
-            }
-            else {
-                white++;
-            }
-            return;
-        }
+    static void dfs(int x, int y, int size) {
         if(check(x, y, size)) {
             return;
         }
-        recur(x, y, size/2);
-        recur(x+size/2, y, size/2);
-        recur(x, y+size/2, size/2);
-        recur(x+size/2, y+size/2, size/2);
+        for(int i = 0; i < 3; i++) {
+            for(int j = 0; j < 3; j++) {
+//                System.out.println((x + j*(size/3)+"\t"+(y + i*(size/3)))+"\t"+size/3);
+                dfs(x + j*(size/3), y + i*(size/3), size/3);
+            }
+        }
     }
 }
