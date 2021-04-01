fb662a36c2d9b7491eb2f5a246d7ad6c9ac47dd5
EunChong <chd830@naver.com>
2021-04-01 14:46:03 +0900

    빗물 stack

--- src/review/acmicpc14719.java
+++ src/review/acmicpc14719.java
@@ -5,6 +5,27 @@ import java.io.*;
 
 // 빗물
 public class acmicpc14719 {
+    static int trap(int[] height) {
+        if (height == null || height.length < 2)
+            return 0;
+
+        Stack<Integer> stack = new Stack<>();
+        int water = 0, i = 0;
+        while (i < height.length) {
+            if (stack.isEmpty() || height[i] <= height[stack.peek()]) {
+                stack.push(i++);
+            } else {
+                int pre = stack.pop();
+                if (!stack.isEmpty()) {
+                    // find the smaller height between the two sides
+                    int minHeight = Math.min(height[stack.peek()], height[i]);
+                    // calculate the area
+                    water += (minHeight - height[pre]) * (i - stack.peek() - 1);
+                }
+            }
+        }
+        return water;
+    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer token = new StringTokenizer(br.readLine());
