ca34d6e2bf9634fcefb2fe6083905a3014687ab2
EunChong <chd830@naver.com>
2021-04-26 13:41:32 +0900

    연산자 끼워넣기 2 수정

--- src/review/acmicpc15658.java
+++ src/review/acmicpc15658.java
@@ -5,53 +5,74 @@ import java.io.*;
 
 // 연산자 끼워넣기(2)
 public class acmicpc15658 {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        nums = new int[N];
-        StringTokenizer token = new StringTokenizer(br.readLine());
-        for(int i = 0; i < N; i++) {
-            nums[i] = Integer.parseInt(token.nextToken());
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            N = Integer.parseInt(br.readLine());
+            nums = new int[N];
+            StringTokenizer token = new StringTokenizer(br.readLine());
+            for(int i = 0; i < N; i++) {
+                nums[i] = Integer.parseInt(token.nextToken());
+            }
+            opers = new int[4];
+            token = new StringTokenizer(br.readLine());
+            for(int i = 0; i < 4; i++) {
+                opers[i] = Integer.parseInt(token.nextToken());
+            }
+            calc(0,  "");
+            System.out.println(max+"\n"+min);
         }
-        opers = new int[4];
-        token = new StringTokenizer(br.readLine());
-        for(int i = 0; i < 4; i++) {
-            opers[i] = Integer.parseInt(token.nextToken());
-        }
-        calc(1, nums[0]);
-        System.out.println(max+"\n"+min);
-    }
-    static int N;
-    static int max = Integer.MIN_VALUE;
-    static int min = Integer.MAX_VALUE;
-    static int[] nums;
-    static int[] opers;
-    static void calc(int idx, int sum) {
-        if(idx == N) {
-            max = Math.max(max, sum);
-            min = Math.min(min, sum);
-//            System.out.println(max+"\t"+min);
-            return;
-        }
-        for(int i = 0; i < opers.length; i++) {
-            if(opers[i] > 0) {
-                opers[i]--;
-                switch (i) {
-                    case 0:
-                        calc(idx+1, sum+nums[idx]);
-                        break;
-                    case 1:
-                        calc(idx+1, sum-nums[idx]);
-                        break;
-                    case 2:
-                        calc(idx+1, sum*nums[idx]);
-                        break;
-                    case 3:
-                        calc(idx+1, sum/nums[idx]);
-                        break;
+        static int N;
+        static int max = Integer.MIN_VALUE;
+        static int min = Integer.MAX_VALUE;
+        static int[] nums;
+        static int[] opers;
+        static void calc(int cnt, String oper) {
+            if(cnt == N-1) {
+                int sum = nums[0];
+//            System.out.print(oper+"\t");
+                for(int i = 0; i < oper.length(); i++) {
+                    char c = oper.charAt(i);
+                    switch (c) {
+                        case '+':
+                            sum += nums[i+1];
+                            break;
+                        case '-':
+                            sum -= nums[i+1];
+                            break;
+                        case '*':
+                            sum *= nums[i+1];
+                            break;
+                        case '/':
+                            sum /= Math.abs(nums[i+1]);
+                            break;
+                    }
                 }
-                opers[i]++;
+                max = Math.max(max, sum);
+                min = Math.min(min, sum);
+//            System.out.println(max+"\t"+min);
+                return;
+            }
+            for(int i = 0; i < opers.length; i++) {
+                if(opers[i] > 0) {
+                    opers[i]--;
+                    String o = "";
+                    switch (i) {
+                        case 0:
+                            o = "+";
+                            break;
+                        case 1:
+                            o = "-";
+                            break;
+                        case 2:
+                            o = "*";
+                            break;
+                        case 3:
+                            o = "/";
+                            break;
+                    }
+                    calc(cnt+1, oper+o);
+                    opers[i]++;
+                }
             }
         }
     }
-}
