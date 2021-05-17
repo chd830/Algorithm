bf8bcc10d930ab5244a12fe436be6fbeaf3b0710
EunChong <chd830@naver.com>
2021-05-17 17:25:00 +0900

    내적

---
+++ src/co/kr/programmers/내적.java
@@ -0,0 +1,11 @@
+package co.kr.programmers;
+
+public class 내적 {
+    public int solution(int[] a, int[] b) {
+        int answer = 0;
+        for(int i = 0; i < a.length; i++) {
+            answer += a[i]*b[i];
+        }
+        return answer;
+    }
+}
