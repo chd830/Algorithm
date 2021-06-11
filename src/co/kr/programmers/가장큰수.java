e3a9545ea8ae4c8474e9de1474aaa2214d4b83d2
EunChong <chd830@naver.com>
2021-06-11 23:43:17 +0900

    가장 큰 수

--- src/co/kr/programmers/가장큰수.java
+++ src/co/kr/programmers/가장큰수.java
@@ -5,31 +5,57 @@ import java.io.*;
 
 public class 가장큰수 {
     public static void main(String[] args) throws IOException {
-        System.out.println(solution(new int[] {6, 10, 2}));
-        System.out.println(solution(new int[] {3, 30, 34, 5, 9}));
+        System.out.println(solution(new int[] {6, 10, 2})); //6210
+        System.out.println(solution(new int[] {3, 30, 34, 5, 9})); //9534330
         System.out.println(solution(new int[] {0, 0, 0, 0}));
     }
     public static String solution(int[] nums) {
-        Integer[] arr = new Integer[nums.length];
-        for(int i = 0; i < nums.length; i++) {
-            arr[i] = nums[i];
-        }
-        Comparator<Integer> comp = new Comparator<Integer>() {
+        List<String> list = new ArrayList<>();
+        for(int n : nums)
+            list.add(""+n);
+        Comparator<String> comp = new Comparator<String>() {
             @Override
-            public int compare(Integer o1, Integer o2) {
-                String s1 = o1.toString();
-                String s2 = o2.toString();
-                return -Integer.compare(Integer.parseInt(s1+s2), Integer.parseInt(s2+s1));
+            public int compare(String o1, String o2) {
+                return -Integer.compare(Integer.parseInt(o1+o2), Integer.parseInt(o2+o1));
             }
         };
-        Arrays.sort(arr, comp);
-        if(arr[0] == 0) {
-            return "0";
+        Collections.sort(list, comp);
+        String res = "";
+        boolean check = true;
+        for(String l : list) {
+            if(!check || !l.equals("0")) {
+                check = false;
+            }
+            res += l;
         }
-        String str = "";
-        for(int i : arr) {
-            str += i;
-        }
-        return str;
+        return check ? "0" : res;
     }
+    // 시간초과
+//    static int[] sub;
+//    static int max;
+//    static boolean[] visited;
+//    public static String solution(int[] nums) {
+//        sub = new int[nums.length];
+//        visited = new boolean[nums.length];
+//        perm(0, nums);
+//        return Integer.toString(max);
+//    }
+//    static void perm(int idx, int[] nums) {
+//        if(idx == sub.length) {
+//            String s = "";
+//            for(int i = 0; i < sub.length; i++) {
+//                s += nums[sub[i]];
+//            }
+//            max = Math.max(max, Integer.parseInt(s));
+//            return;
+//        }
+//        for(int i = 0; i < sub.length; i++) {
+//            if(!visited[i]) {
+//                visited[i] = true;
+//                sub[idx] = i;
+//                perm(idx+1, nums);
+//                visited[i] = false;
+//            }
+//        }
+//    }
 }
