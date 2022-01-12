83f645cf038136d1e1c878cfb51161c14a2dedf9
EunChong <chd830@naver.com>
2022-01-12 19:38:22 +0900

    파일정리

---
+++ src/sort/파일정리.java
@@ -0,0 +1,26 @@
+package sort;
+
+import java.util.*;
+import java.io.*;
+
+// https://www.acmicpc.net/problem/20291
+public class 파일정리 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        String[] files = new String[N];
+        TreeMap<String, Integer> map = new TreeMap<>();
+        for(int i = 0; i < N; i++) {
+            files[i] = br.readLine();
+            String extension = files[i].split("\\.")[1];
+            if(map.containsKey(extension))
+                map.put(extension, map.get(extension)+1);
+            else
+                map.put(extension, 1);
+        }
+        StringBuilder sb = new StringBuilder();
+        for(String key : map.keySet())
+            sb.append(key).append(" ").append(map.get(key)).append("\n");
+        System.out.println(sb.toString());
+    }
+}
