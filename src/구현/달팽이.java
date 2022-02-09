5aa4cf4b1d460634dd6461256335738fd23bc809
EunChong <chd830@naver.com>
2022-02-09 18:17:20 +0900

    달팽ㅇ;

--- src/구현/달팽이.java
+++ src/구현/달팽이.java
@@ -3,53 +3,103 @@ package 구현;
 import java.util.*;
 import java.io.*;
 
-// 돌아가면서 배열채우기
+// https://www.acmicpc.net/problem/1913
 public class 달팽이 {
-    public static void main(String[] args) {
-        N = 7;
-        cnt = 1;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        find = Integer.parseInt(br.readLine());
+        idx = 1;
         arr = new int[N][N];
-//        snail(0);
+        x = N/2;
+        y = N/2;
+        arr[x][y] = idx++;
+        x--;
+        arr[x][y] = idx++;
+        y++;
+        snail();
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < N; j++) {
+                sb.append(arr[i][j]).append(" ");
+            }
+            sb.append("\n");
+        }
+        if(find == 1) {
+            resultX = N/2;
+            resultY = N/2;
+        }
+        else if(find == 2) {
+            resultX = N/2-1;
+            resultY = N/2;
+        }
+        sb.append(resultX+1).append(" ").append(resultY+1);
+        System.out.println(sb.toString());
     }
     static int[][] arr;
+    static int find;
+    static int idx;
+    static int N;
     static int x;
     static int y;
-    static int cnt;
-    static int N;
-//    static void snail(int idx) {
-//        boolean check = false;
-//        for(int i = 0; i < N; i++) {
-//            if(arr[idx][i] != 0)
-//                continue;
-//            arr[idx][i] = cnt++;
-//            check = true;
-//        }
-//        for(int i = 0; i < N; i++) {
-//            if(arr[i][N-1-idx] != 0)
-//                continue;
-//            arr[i][N-1-idx] = cnt++;
-//            check = true;
-//        }
-//        for(int i = 0; i < N; i++) {
-//            if(arr[N-1-idx][N-1-i] != 0)
-//                continue;
-//            arr[N-1-idx][N-1-i] = cnt++;
-//            check = true;
-//        }
-//        for(int i = 0; i < N; i++) {
-//            if(arr[N-1-i][idx] != 0)
-//                continue;
-//            arr[N-1-i][idx] = cnt++;
-//            check = true;
-//        }
-//        print();
-//        if(check) {
-//            snail(idx+1);
-//        }
-//    }
-    static void print() {
-        for(int i = 0; i < N; i++)
-            System.out.println(Arrays.toString(arr[i]));
-        System.out.println();
+    static int resultX;
+    static int resultY;
+
+    static void snail() {
+        int cnt = 2;
+        while(cnt+1 <= N) {
+            down(cnt+1);
+            left(cnt);
+            if(cnt+1 == N)
+                up(cnt);
+            else
+                up(cnt+1);
+            if(cnt+1 != N)
+                right(cnt);
+            cnt += 2;
+        }
+    }
+
+    static void down(int cnt) {
+        for(int i = 0; i < cnt; i++) {
+            if(idx == find) {
+                resultX = x;
+                resultY = y;
+            }
+            arr[x++][y] = idx++;
+        }
+        x--;
+        y--;
+    }
+    static void left(int cnt) {
+        for(int i = 0; i < cnt; i++) {
+            if(idx == find) {
+                resultX = x;
+                resultY = y;
+            }
+            arr[x][y--] = idx++;
+        }
+        x--;
+        y++;
+    }
+    static void up(int cnt) {
+        for(int i = 0; i < cnt; i++) {
+            if(idx == find) {
+                resultX = x;
+                resultY = y;
+            }
+            arr[x--][y] = idx++;
+        }
+        x++;
+        y++;
+    }
+    static void right(int cnt) {
+        for(int i = 0; i < cnt; i++) {
+            if(idx == find) {
+                resultX = x;
+                resultY = y;
+            }
+            arr[x][y++] = idx++;
+        }
     }
 }
