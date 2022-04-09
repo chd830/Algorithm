187e0c4f899dfbe993597100e3716f4d6931b009
EunChong <chd830@naver.com>
2022-04-09 23:41:49 +0900

    팩토리얼 0의 개수

---
+++ src/구현/팩토리얼0의개수.java
@@ -0,0 +1,17 @@
+package 구현;
+
+import java.util.*;
+import java.io.*;
+
+public class 팩토리얼0의개수 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int answer = 0;
+        while(N >= 5) {
+            answer += N/5;
+            N /= 5;
+        }
+        System.out.println(answer);
+    }
+}
