628b6a0b9e001e9fad625100749981c434f2a009
EunChong <chd830@naver.com>
2022-04-30 17:24:02 +0900

    LCS

---
+++ src/Dynamic/LCS.java
@@ -0,0 +1,25 @@
+package Dynamic;
+
+import java.util.*;
+import java.io.*;
+
+// https://acmicpc.net/problem/9251
+public class LCS {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s1 = br.readLine();
+        String s2 = br.readLine();
+        int[][]dp = new int[s1.length()+1][s2.length()+1];
+        int max = 0;
+        for(int i = 1; i <= s1.length(); i++) {
+            for(int j = 1; j <= s2.length(); j++) {
+                if(s1.charAt(i-1) != s2.charAt(j-1))
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                else
+                    dp[i][j] = dp[i-1][j-1]+1;
+                max = Math.max(max, dp[i][j]);
+            }
+        }
+        System.out.println(max);
+    }
+}
