6f5db4d624b47e8411db537ecf20dbb3c0b6eb28
EunChong <chd830@naver.com>
2022-05-03 17:43:17 +0900

    경비원

--- src/구현/경비원.java
+++ src/구현/경비원.java
@@ -13,31 +13,40 @@ public class 경비원 {
             this.y = y;
         }
     }
+    // 1 북 2 남 3 서 4 동
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer token = new StringTokenizer(br.readLine());
         int M = Integer.parseInt(token.nextToken());
         int N = Integer.parseInt(token.nextToken());
-        int num = Integer.parseInt(br.readLine());
-        Node[] shop = new Node[num];
-        for(int i = 0; i < num; i++) {
+        int[][] arr = new int[N+1][M+1];
+        int n = Integer.parseInt(br.readLine());
+        Node[] shops = new Node[n];
+        for(int i = 0; i <= N; i++)
+            Arrays.fill(arr[i], Integer.MAX_VALUE);
+        for(int i = 0; i < n; i++) {
             token = new StringTokenizer(br.readLine());
-            switch (Integer.parseInt(token.nextToken())) {
+            int direction = Integer.parseInt(token.nextToken());
+            int position = Integer.parseInt(token.nextToken());
+            switch (direction) {
                 case 1:
-                    shop[i] = new Node(0, Integer.parseInt(token.nextToken()));
+                    shops[i] = new Node(0, position);
+                    arr[0][position] = -1;
                     break;
                 case 2:
-                    shop[i] = new Node(N-1, Integer.parseInt(token.nextToken()));
+                    shops[i] = new Node(N, position);
+                    arr[N][position] = -1;
                     break;
                 case 3:
-                    shop[i] = new Node(Integer.parseInt(token.nextToken()), 0);
+                    shops[i] = new Node(position, 0);
+                    arr[position][0] = -1;
                     break;
                 case 4:
-                    shop[i] = new Node(Integer.parseInt(token.nextToken()), N-1);
+                    shops[i] = new Node(position, M);
+                    arr[position][M] = -1;
                     break;
             }
         }
-
         token = new StringTokenizer(br.readLine());
         Node start = null;
         switch (Integer.parseInt(token.nextToken())) {
@@ -45,58 +54,42 @@ public class 경비원 {
                 start = new Node(0, Integer.parseInt(token.nextToken()));
                 break;
             case 2:
-                start = new Node(N-1, Integer.parseInt(token.nextToken()));
+                start = new Node(N, Integer.parseInt(token.nextToken()));
                 break;
             case 3:
                 start = new Node(Integer.parseInt(token.nextToken()), 0);
                 break;
             case 4:
-                start = new Node(Integer.parseInt(token.nextToken()), N-1);
+                start = new Node(Integer.parseInt(token.nextToken()), M);
                 break;
         }
-        int sum = 0;
-        for(Node n : shop) {
-            if(n.x == 0) {
-                if(start.x == 0)
-                    sum += Math.abs(n.y-start.y);
-                else if(start.y == 0)
-                    sum += start.x+n.y;
-                else if(start.x == N-1)
-                    sum += Math.min(start.y+N+n.y, M-start.y+N+M-n.y);
-                else
-                    sum += start.x+M-n.y;
-            }
-            else if(n.y == 0) {
-                if(start.x == 0)
-                    sum += start.x+n.y;
-                else if(start.y == 0)
-                    sum += Math.abs(start.x-n.x);
-                else if(start.x == N-1)
-                    sum += start.y+N-n.x;
-                else
-                    sum += Math.min(start.x+M+n.x, N-start.x+M+N-n.y);
-            }
-            else if(n.x == N-1) {
-                if(start.x == 0)
-                    sum += Math.min(start.y+N+n.y, M-start.y+N+M-n.y);
-                else if(start.y == 0)
-                    sum += n.y+N-start.x;
-                else if(start.x == N-1)
-                    sum += Math.abs(n.y-start.y);
-                else
-                    sum += M-n.y+N-start.x;
-            }
-            else {
-                if(start.x == 0)
-                    sum += n.x+M-start.y;
-                else if(start.y == 0)
-                    sum += Math.min(start.x+M+n.x, N-start.x+M+N-n.x);
-                else if(start.x == N-1)
-                    sum += N-n.x+M-start.y;
-                else
-                    sum += Math.abs(n.x-start.x);
+        Queue<Node> que = new LinkedList<>();
+        que.add(start);
+        arr[start.x][start.y] = 0;
+        while(!que.isEmpty()) {
+            Node node = que.poll();
+            for(int d = 0; d < 4; d++) {
+                int dx = node.x + dir[d][0];
+                int dy = node.y + dir[d][1];
+                if((dx >= 0 && dy >= 0 && dx <= N && dy <= M) && (dx == 0 || dy == 0 || dx == N || dy == M)) {
+                    if(arr[dx][dy] == -1 || arr[dx][dy] > arr[node.x][node.y]+1) {
+                        arr[dx][dy] = arr[node.x][node.y]+1;
+                        que.add(new Node(dx, dy));
+                    }
+                }
             }
         }
-        System.out.println(sum);
+        int answer = 0;
+        for(int i = 0; i < n; i++) {
+            answer += arr[shops[i].x][shops[i].y];
+        }
+//        for(int i = 0; i <= N; i++) {
+//            for(int j = 0; j <= M; j++) {
+//                System.out.print(arr[i][j] == Integer.MAX_VALUE ? "\t" : arr[i][j]+"\t");
+//            }
+//            System.out.println();
+//        }
+        System.out.println(answer);
     }
+    static int[][] dir = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
 }
